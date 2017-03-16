6c5e8f3613923f9bd8aa5a19cbdf5ca25a4566bd
Shweta Mahajan <mahajan.shweta112@gmail.com>
2017-03-16 18:50:04 +0100

    Verbose update

--- try_outs/keras/keras_first_network.py
+++ try_outs/keras/keras_first_network.py
@@ -19,14 +19,14 @@ model.add(Dense(1, init='uniform', activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fitting the model
-model.fit(X, Y, nb_epoch=150, batch_size=10)
+model.fit(X, Y, nb_epoch=150, batch_size=10) # verbose=0 to turn off progress bars
 
 # Evaluation
-scores = model.evaluate(X, Y)
-print(' %s: %.2f%%'%(model.metrics_names[1], scores[1]*100))
+scores = model.evaluate(X, Y) # verbose=0 to turn off progress bars
+print('%s: %.2f%%'%(model.metrics_names[1], scores[1]*100))
 
-# calculate predictions
+# Calculate predictions
 predictions = model.predict(X)
-# round predictions
+# Round predictions
 rounded = [round(x[0]) for x in predictions]
 print(rounded)
